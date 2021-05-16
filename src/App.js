5bd1795f01c25192eac63bd95c48c703d6611639
vikky <vikkyhp0007@gmail.com>
2021-05-16 17:22:59 +0530

    redux

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// components
+import Navbar from "./components/Navbar";
+import CartContainer from "./components/CartContainer";
+// items
+import cartItems from "./cart-items";
+// redux stuff
+
+// store - stores data, think of state
+// reducer - function that used to update/ how to do?
+// two arguments - state, action
+// state - old state/ state before update
+// action - what happened/ what update/ what to do? 
+// return updated or old state
+
+// -----------------********------------------
+
+// creating a store
+import { createStore } from "redux";
+// reducer
+function reducer() {
+  console.log("shake and bake");
+}
+
+const store = createStore(reducer);
 
 function App() {
+  // cart setup
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Navbar />
+      <CartContainer cart={cartItems} />
+    </main>
   );
 }
 
